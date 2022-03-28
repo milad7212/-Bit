a4923467a28b17c74e1c2edd84fa4fbfac323fdf
miillaadddddd <milad7212@gmail.com>
2022-03-28 12:37:45 +0430

    init

---
+++ pages/_app.jsx
@@ -0,0 +1,7 @@
+import "../styles/globals.css";
+import variables from "../styles/variables.module.scss";
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
