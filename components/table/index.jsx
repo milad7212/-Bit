18c48b7bc5001ea808987956239bb04495969d0a
miillaadddddd <milad7212@gmail.com>
2022-03-28 23:55:42 +0430

    Add coin Cell in table

--- components/table/index.jsx
+++ components/table/index.jsx
@@ -1,11 +1,31 @@
+import Image from "next/image";
 import React from "react";
 
 function Table({ data }) {
   return (
-    <div>
-      {data.result.items.map((e) => (
+    <div style={{direction:'ltr'}}>
+      {data.result.items.map((item,index) => (
         <>
-          <div>{e.faName}</div>
+          <div className="flex">
+            <div className="flex flex-grow">
+              <div className="">
+                <Image src={item.icon} layout='fixed' height={32} width={32} alt=''/>
+              </div>
+              <div className="">
+                <h2>{item.enName}</h2>
+                <div className="flex">
+                  <span>{index+1}</span>
+                  <span>{item.coin}</span>
+                </div>
+              </div>
+              
+            
+            </div>
+            <div className="">{item.faName}</div>
+            <div className="">{item.faName}</div>
+            <div className="">{item.faName}</div>
+            
+            </div>
         </>
       ))}
     </div>
