8f692b318c7a6ccdd5621c0893e4943db7221df4
miillaadddddd <milad7212@gmail.com>
2022-03-28 13:56:46 +0430

    Add TailwindCss

---
+++ components/menuHeader/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MenuHeader() {
+  return <div className="">
+      <div className=""></div>
+      
+      
+      MenuHeader</div>;
+}
+
+export default MenuHeader;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,4 @@
+import MenuHeader from "../components/menuHeader";
 import s from "./mainPage.module.scss";
 export default function Home() {
   return (
