a148e84a26c400c9617ee73319c6d662ab637536
miillaadddddd <milad7212@gmail.com>
2022-03-29 11:02:13 +0430

    Add header to page

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,10 +2,15 @@ import MenuHeader from "../components/menuHeader";
 import Table from "../components/table";
 import { dataApiStatic } from "../components/data.js";
 import SettingTable from "../components/SettingTable/SettingTable";
+import Head from "next/head";
 export default function Home() {
   console.log("dataApiStatic :>> ", dataApiStatic.result);
   return (
     <>
+      <Head>
+        <title>قیمت لحظه ای | بیت برگ</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <MenuHeader />
       <div className="pt-20">
         <SettingTable />
